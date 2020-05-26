7a02bb9d3965411ab3665e7d094f7e15182783a8
Martín López Paglione <martincholp@hotmail.com>
2020-05-26 16:23:55 -0300

    Manlog v1 terminado

--- manlog.py
+++ manlog.py
@@ -16,32 +16,9 @@ class ArchivoLog(object):
         self.__modificado = False # Indica si se ha realizado algún cambio que todavía no está actualizado
                                   
         self.__formato = "%d/%m/%Y-%H:%M:%S.%f"  # Formato de fecha y hora. Por defecto es " dd/mm/aaaa-HH:MM:SS.ssssss "
-                                                 # Se puede generar formatos alternativos usando la siguiente tabla:
+                                                 # Para generar formatos alternativos ver docstring en la propiedad formato
 
-                        #  Símbolo          Significado                                                   Ejemplo
-                        #    %a       Día de la semana abreviado                                           'Wed'
-                        #    %A       Nombre completo del día de la semana                                 'Wednesday'
-                        #    %w       Número del día de la semana – 0 (Domingo) al 6 (Sábado)              '3'
-                        #    %d       Día del mes (rellenado con cero)                                     '13'
-                        #    %b       Nombre del mes abreviado                                             'Jan'
-                        #    %B       Nombre completo del mes                                              'January'
-                        #    %m       Mes del año                                                          '01'
-                        #    %y       Año sin siglo                                                        '16'
-                        #    %Y       Año con siglo                                                        '2016'
-                        #    %H       Hora del reloj de 24 horas                                           '17'
-                        #    %I       Hora del reloj de 12 horas                                           '05'
-                        #    %p       AM/PM                                                                'PM'
-                        #    %M       Minutos                                                              '00'
-                        #    %S       Segundos                                                             '00'
-                        #    %f       Microsegundos                                                        '000000'
-                        #    %z       Desplazamiento UTC para objetos con reconocimiento de zona horaria   '-0500'
-                        #    %Z       Nombre de la zona horaria                                            'EST'
-                        #    %j       Día del año                                                          '013'
-                        #    %W       Semana del año                                                       '02'
-                        #    %c       Representación de fecha y hora para el lugar presente                'Wed Jan 13 17:00:00 2016'
-                        #    %x       Representación de fecha para el lugar presente                       '01/13/16'
-                        #    %X       Representación de hora para el lugar presente                        '17:00:00'
-                        #    %%       Un caractér % literal                                                '%'
+                       
 
     @property
     def formato(self):
@@ -96,7 +73,7 @@ class ArchivoLog(object):
     
     @property
     def registros(self):
-        '''Lista de los registros de la base de datos'''
+        '''Lista con los registros de la base de datos'''
 
         lista = self.__regs.copy()
         nuevos = self.__regsNuevos.copy()
@@ -105,16 +82,53 @@ class ArchivoLog(object):
     
 
 
-    def cargarArchivo():
-        '''Carga el archivo indicado en self.__archivo'''
-        pass
+    def cargarArchivo(self):
+        '''Carga el archivo indicado en self.__archivo. El separador de los datos del registro y el formato de
+        fecha utilizado será el indicado en self.separador y self.formato. Devuelve True si el archivo existe
+        o False si no puede encontrar el archivo.'''
+
+        try:
+            f = None
+            f = open(self.__archivo, 'r')
+  
+        except FileNotFoundError:
+            return False
+                      
+        else:
+            for linea in f:
+                fechahora, evento, argumentos, comentarios = linea.strip().split(self.separador)
+                self.agregarRegistro(Registro(evento, argumentos, comentarios, fechahora, self.formato), False)
+
+            # Al usar agregarRegistro se la variable __modificado se hace True, por lo tanto tenemos que  volverla a False manualmente
+            self.__modificado = False
+            return True
+        
+        finally:
+            if not (f==None):
+                f.close()
+
+   
     
-    
-    def buscarRegistros():
+    def buscarRegistros(self, campo, cadena):
         '''Buscar registros segun algun filtro indicado. Devuelve una lista con los resultados'''
-        pass
+
+        # Actualmente solo busca coincidencias. En vesiones futuras usar regex
+
+        lista = self.registros  # Lista con todos los registros cargados (los del archivos y los que aún no fueron actualizados)
+        resultados = []
+        
+
+        for r in lista:
+	        if campo == 'fechahora': # Caso especial si el campo s 'fechahora'
+	        	if cadena in getattr(r, 'strFechahora')(self.formato):
+	        		resultados.append(r)
+
+	        elif campo in ['evento', 'argumentos', 'comentarios']:  # Evito que se pueda buscar otra cosa. Solo estos campos estan disponibles para buscar
+	        	if cadena in getattr(r, campo):
+	        		resultados.append(r)
 
 
+        return resultados
 
 
     def agregarRegistro(self, registro, actualizar=True):
@@ -142,35 +156,35 @@ class ArchivoLog(object):
         self.aux = []
 
         try:
-            print('try')
+            #print('try')
             self.aux = self.__regs.copy()  # Copia los registros en una lista auxiliar
             self.__regsNuevos.sort()            # ordenados por fecha y hora, por las dudas...
             self.__regs.extend(self.__regsNuevos)  # Pongo los nuevos en la lista principal
 
             f = open(self.__archivo, 'a')  # intento abrir el archivo para agregar lineas
-            print('/try')
+            #print('/try')
 
         except FileNotFoundError:
-            print('except1')
+            #print('except1')
             resultado = False     # si el archivo no existe devuelvo falso
-            print('/except1')
+            #print('/except1')
 
         except:  # si es otra excepcion la lanzo 
-            print('except2')
+            #print('except2')
             raise
-            print('/except2')
+            #print('/except2')
 
         else:    # si pude abrir el archivo guardo los registros nuevos
-            print('else')
+            #print('else')
 
             for reg in self.__regsNuevos:
                 f.write(reg.reg2str(self.formato, self.separador) + '\n')
 
             resultado = True
 
-            print('/else')
+            #print('/else')
         finally:
-            print('finally')
+            #print('finally')
 
             if resultado: # si pude guardar todos los registros 
                 f.close()  #cierro el archivo
@@ -180,7 +194,7 @@ class ArchivoLog(object):
             else:  # Si no se pudo actualizar
                 self.__regs = self.aux  # vuelvo el estado de __regs al valor antes del __regs.extend()
 
-            print('/finally')
+            #print('/finally')
             # devuelvo el resultado
             return resultado
 
@@ -190,16 +204,20 @@ class ArchivoLog(object):
 
 class Registro(object):
     """Clase que representa un registro dentro de un archivo"""
-    def __init__(self, evento, argumentos="", comentarios=""):
+    def __init__(self, evento, argumentos="", comentarios="", fechahora="", formatoFecha='%d/%m/%Y-%H:%M:%S.%f'):
 
-        self.__fechahora = datetime.datetime.now()  # La fecha y hora se debe aregar automáticamente, y es en el momento en que se crea el objeto
+        if fechahora == "":
+            self.__fechahora = datetime.datetime.now()  # Si no se especifica fecha y hora se debe aregar automáticamente, y es en el momento en que se crea el objeto
+        else:
+            self.__fechahora = datetime.datetime.strptime(fechahora, formatoFecha)  # Parsea fecha y hora según el formato pasado
+            
         self.__evento = evento
         self.__argumentos = argumentos
-        self.__comentarios=comentarios
+        self.__comentarios = comentarios
         
 
     def __lt__(self, reg):
-        # Compara reg con self y devuelve True si self.fechahora es menor que reg.fechahora, si no devuelve False
+        '''Compara reg con self y devuelve True si self.fechahora es menor que reg.fechahora, si no devuelve False'''
 
         if self.__fechahora < reg.fechahora:
             return True
@@ -207,7 +225,7 @@ class Registro(object):
             return False
 
     def __gt__(self, reg):
-        # Compara reg con self y devuelve True si self.fechahora es mayor que reg.fechahora, si no devuelve False
+        '''Compara reg con self y devuelve True si self.fechahora es mayor que reg.fechahora, si no devuelve False'''
 
         if self.__fechahora > reg.fechahora:
             return True
@@ -215,12 +233,16 @@ class Registro(object):
             return False
 
     def __eq__(self, reg):
-        # Compara reg con self y devuelve True si self.fechahora es igual a reg.fechahora, si no devuelve False
+        '''Compara reg con self y devuelve True si self.fechahora es igual a reg.fechahora, si no devuelve False'''
 
         if self.__fechahora > reg.fechahora:
             return True
         else:
             return False
+
+    def __str__(self):
+        '''Devuelve el registro como string '''
+        return self.reg2str()
 
     @property
     def fechahora(self):
@@ -242,16 +264,14 @@ class Registro(object):
         '''Devuelve comentarios asociados al registro'''
         return self.__comentarios
 
-    def reg2str(self, formato, separador=''):
-        '''Devuelve el registro como un string, con la fecha en el formato pasado. Útil para escribir el archivo. 
+    def reg2str(self, formatoFecha='%d/%m/%Y-%H:%M:%S.%f', separador='#'):
+        '''Devuelve el registro como un string, muy útil para escribir en el archivo. 
+        Si no se especifica el formato de fecha se utiliza el formato por defecto ( %d/%m/%Y-%H:%M:%S.%f )
         Para separar entre los campos del registro se utiliza separador. Si no se especifica se usa el caracter #'''
 
-        if separador == '':
-            separador = '#'
+        return  self.strFechahora(formatoFecha) + separador + self.evento + separador + self.argumentos + separador + self.comentarios
 
-        return  self.strFechahora(formato) + separador + self.evento + separador + self.argumentos + separador + self.comentarios
-
-    def strFechahora(self, formato = "%d/%m/%Y-%H:%M:%S.%f"):
+    def strFechahora(self, formatoFecha = '%d/%m/%Y-%H:%M:%S.%f'):
         '''Devuelve la fecha y hora como un string, con el formato pasado'''
-        return self.__fechahora.strftime(formato)
+        return self.__fechahora.strftime(formatoFecha)
 
